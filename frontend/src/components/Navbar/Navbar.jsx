40a65a43395429f6769f2a19f04660e46eaf1a6c
Sachin Rajput <sachinrajput988724@gmail.com>
2025-03-22 01:22:17 +0530

    navbar correction

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,91 +1,275 @@
-import React from 'react'
+import React from "react";
 import "./Navbar.css";
-import menuicon from '../../assets/icon/menu_icon.png'
-import menuCloseIcon from '../../assets/icon/menu_close.png'
-import darkmodeicon from '../../assets/icon/dark_mode.png'
-import lightmodeicon from '../../assets/icon/light_mode.png'
+import menuicon from "../../assets/icon/menu_icon.png";
+import menuCloseIcon from "../../assets/icon/menu_close.png";
+import darkmodeicon from "../../assets/icon/dark_mode.png";
+import lightmodeicon from "../../assets/icon/light_mode.png";
 
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-
-
-
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect,useRef } from "react";
 
 export default function Navbar(props) {
+  const navRef = useRef(null);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   
-  function HamBurgerMode(){
+
+  function HamBurgerMode() {
     setIsMenuOpen(!isMenuOpen);
 
-    let can=document.querySelector('.container-fluid');
-    let ham=document.querySelector('.navbar-toggler');
-    let menu=document.querySelector('.navbar-menu');
-    let form=document.querySelector('.form-for-search');
-    let disflex = document.querySelector('.displayflex');
-    let sign=document.querySelector('.sign-in-button');
-    let mode=document.querySelector('.mode-valid');
-    let title=document.querySelector('.navbar-title');
-    let body=document.querySelector('body');
-    ham.classList.toggle('active');
-    !can.classList.toggle('active');
-    menu.classList.toggle('active');
-    !disflex.classList.toggle('active');
-    form.classList.toggle('active');
-    sign.classList.toggle('active');
-    mode.classList.toggle('active');
-    title.classList.toggle('active');
-    body.classList.toggle('active');
+    let can = document.querySelector(".container-fluid");
+    let ham = document.querySelector(".navbar1-toggler");
+    let menu = document.querySelector(".navbar1-menu");
+    let form = document.querySelector(".form-for-search");
+    let disflex = document.querySelector(".displayflex");
+    let sign = document.querySelector(".sign-in-button");
+    // let mode=document.querySelector('.form-for-darkmode-lightmode');
+    let title = document.querySelector(".navbar1-title");
+    let body = document.querySelector("body");
+    ham.classList.toggle("active");
+    !can.classList.toggle("active");
+    menu.classList.toggle("active");
+    !disflex.classList.toggle("active");
+    form.classList.toggle("active");
+    sign.classList.toggle("active");
+    // mode.classList.toggle('active');
+    title.classList.toggle("active");
+    body.classList.toggle("active");
 
+
+  }
+  useEffect(() => {
+    if (navRef.current) {
+      navRef.current.addEventListener("wheel", (e) => {
+        e.preventDefault();
+        navRef.current.scrollLeft += e.deltaY;
+      });
     }
+  }, []); // Runs once after the component mounts
+  
   return (
-    <nav className={`navbar   bg-${props.Mode}  text-${props.Mode==='light'?'black':'white'} `} >
-      <div className="container-fluid">
-        <a className={`navbar-title text-${props.Mode==='light'?'black':'white'}`} href="/">{props.title}</a>
-        <div className={`collapse   `} id="navbarSupportedContent">
-          <ul className="navbar-menu ">
-            <li className="nav-item">
-              <Link className={`nav-link  text-${props.Mode==='light'?'dark':'light'}`} aria-current="page" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <a className={`nav-link text-${props.Mode==='light'?'dark':'light'}`} href="/">Link</a>
-            </li>
-            <li className="category-dropdown">
-              <li className={`nav-link  text-${props.Mode==='light'?'dark':'light'}`}  role="button" >
-                Category
+    <>
+      <nav
+        className={`navbar1   bg-${props.Mode}  text-${
+          props.Mode === "light" ? "black" : "white"
+        } `}
+      >
+        <div className="container-fluid">
+          <a
+            className={`navbar1-title text-${
+              props.Mode === "light" ? "black" : "white"
+            }`}
+            href="/"
+          >
+            {props.title}
+          </a>
+          <div className={`collapse   `} id="navbarSupportedContent">
+            <ul className="navbar1-menu ">
+              <li className="nav-item">
+                <Link
+                  className={`nav-link  text-${
+                    props.Mode === "light" ? "dark" : "light"
+                  }`}
+                  aria-current="page"
+                  to="/"
+                >
+                  Home
+                </Link>
               </li>
-              <ul className={`dropdown-menu bg-${props.Mode}`} >
-                <li><Link className={`dropdown-item bg-${props.Mode} text-${props.Mode==='light'?'black':'white'}`} to="/general">General</Link></li>
-                <li><Link className={`dropdown-item bg-${props.Mode} text-${props.Mode==='light'?'black':'white'}`} to="/entertainment">Entertainment</Link></li>
-                <li><Link className={`dropdown-item bg-${props.Mode} text-${props.Mode==='light'?'black':'white'}`} to="/health">Health</Link></li>
-                <li><Link className={`dropdown-item bg-${props.Mode} text-${props.Mode==='light'?'black':'white'}`} to="/science">Science</Link></li>
-                <li><Link className={`dropdown-item bg-${props.Mode} text-${props.Mode==='light'?'black':'white'}`} to="/sports">Sports</Link></li>
-                <li><Link className={`dropdown-item bg-${props.Mode} text-${props.Mode==='light'?'black':'white'}`} to="/technology">Technology</Link></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <Link className={`nav-link  text-${props.Mode==='light'?'dark':'light'}`}   to="/about">About Us</Link>
-            </li>
-          </ul> 
+              <li className="nav-item">
+                <a
+                  className={`nav-link text-${
+                    props.Mode === "light" ? "dark" : "light"
+                  }`}
+                  href="/"
+                >
+                  Link
+                </a>
+              </li>
+              <li className="category-dropdown">
+                <li
+                  className={`nav-link  text-${
+                    props.Mode === "light" ? "dark" : "light"
+                  }`}
+                  role="button"
+                >
+                  Category
+                </li>
+                <ul className={`dropdown-menu bg-${props.Mode}`}>
+                  <li>
+                    <Link
+                      className={`dropdown-item bg-${props.Mode} text-${
+                        props.Mode === "light" ? "black" : "white"
+                      }`}
+                      to="/general"
+                    >
+                      General
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`dropdown-item bg-${props.Mode} text-${
+                        props.Mode === "light" ? "black" : "white"
+                      }`}
+                      to="/entertainment"
+                    >
+                      Entertainment
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`dropdown-item bg-${props.Mode} text-${
+                        props.Mode === "light" ? "black" : "white"
+                      }`}
+                      to="/health"
+                    >
+                      Health
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`dropdown-item bg-${props.Mode} text-${
+                        props.Mode === "light" ? "black" : "white"
+                      }`}
+                      to="/science"
+                    >
+                      Science
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`dropdown-item bg-${props.Mode} text-${
+                        props.Mode === "light" ? "black" : "white"
+                      }`}
+                      to="/sports"
+                    >
+                      Sports
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`dropdown-item bg-${props.Mode} text-${
+                        props.Mode === "light" ? "black" : "white"
+                      }`}
+                      to="/technology"
+                    >
+                      Technology
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={`nav-link  text-${
+                    props.Mode === "light" ? "dark" : "light"
+                  }`}
+                  to="/about"
+                >
+                  About Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="displayflex search-signin-darkmode">
+            <form className="form-for-search" role="search">
+              <input
+                className="form-for-search-input"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              {/* <button className="form-for-search-button" type="search">Search</button> */}
+            </form>
+
+            <button className="sign-in-button" type="submit">
+              Sign-in
+            </button>
+            <div
+              className={`form-for-darkmode-lightmode  text-${
+                props.Mode === "light" ? "dark" : "light"
+              }`}
+            >
+              <img
+                className={`mode-${
+                  props.Mode === "light" ? "valid" : "invalid"
+                }`}
+                onClick={props.toggleMode}
+                src={darkmodeicon}
+                alt=""
+              />
+              <img
+                className={`mode-${
+                  props.Mode === "dark" ? "valid" : "invalid"
+                }`}
+                onClick={props.toggleMode}
+                src={lightmodeicon}
+                alt=""
+              />
+            </div>
+          </div>
+          <button
+            onClick={HamBurgerMode}
+            className={`navbar1-toggler bg-${
+              props.Mode === "light" ? "" : "white"
+            }`}
+            type="button"
+          >
+            <img
+              src={isMenuOpen ? menuCloseIcon : menuicon}
+              alt="Menu Toggle"
+            />
+          </button>
         </div>
-        <div className='displayflex search-signin-darkmode'>
-        <form className="form-for-search" role="search">
-          <input className="form-for-search-input" type="search"  placeholder="Search" aria-label="Search"/>
-          {/* <button className="form-for-search-button" type="search">Search</button> */}
-        </form>
-        <div className='displayflex'>
-        <button className='sign-in-button' type='submit'>Sign-in</button>
-        <div className={`form-for-darkmode-lightmode text-${props.Mode==='light'?'dark':'light'}`} >
-          <img className={`mode-${props.Mode==='light'?'valid':'invalid'}`} onClick={props.toggleMode} src={darkmodeicon} alt="" />
-          <img className={`mode-${props.Mode!='light'?'valid':'invalid'}`} onClick={props.toggleMode} src={lightmodeicon} alt="" />
+      </nav>
+      <nav className="navbar2">
+        <div className="nav-container2">
+          <div className="nav-item2">
+            TVs & Appliances ▼
+            <div className="dropdown2">
+              <a href="#">Smart TVs</a>
+              <a href="#">Washing Machines</a>
+            </div>
+          </div>
+          <div className="nav-item2">
+            Men ▼
+            <div className="dropdown2">
+              <a href="#">Shirts</a>
+              <a href="#">Shoes</a>
+            </div>
+          </div>
+          <div className="nav-item2">
+            Women ▼
+            <div className="dropdown2">
+              <a href="#">Dresses</a>
+              <a href="#">Jewelry</a>
+            </div>
+          </div>
+          <div className="nav-item2">
+            Baby & Kids ▼
+            <div className="dropdown2">
+              <a href="#">Toys</a>
+              <a href="#">Clothes</a>
+            </div>
+          </div>
+          <div className="nav-item2">
+            Home & Furniture ▼
+            <div className="dropdown2">
+              <a href="#">Sofas</a>
+              <a href="#">Beds</a>
+            </div>
+          </div>
+          <div className="nav-item2">
+            Sports, Books & More ▼
+            <div className="dropdown2">
+              <a href="#">Cricket</a>
+              <a href="#">Novels</a>
+            </div>
+          </div>
+          <div className="nav-item2">Flights</div>
+          <div className="nav-item2">Offer Zone</div>
         </div>
-        </div>
-        </div>
-        <button onClick={HamBurgerMode} className={`navbar-toggler bg-${props.Mode==='light'?'':'white'}`} type="button" >
-        <img src={isMenuOpen ? menuCloseIcon : menuicon} alt="Menu Toggle" />
-        </button>
-        
-      </div>
-      
-    </nav>
-  )
+      </nav>
+    </>
+  );
 }
